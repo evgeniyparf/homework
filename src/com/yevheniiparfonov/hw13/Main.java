43cca7d1edd071955ba97ae85e0e8c042f033593
evgeniyparf <evgeniy.parf@gmail.com>
2018-11-07 16:10:10 +0200

    exceptions handling

--- src/com/yevheniiparfonov/hw13/Main.java
+++ src/com/yevheniiparfonov/hw13/Main.java
@@ -5,24 +5,38 @@ public class Main {
     classCastException(10);
     nullPointerException(null);
     numberFormatException("hello world");
-    arrayIndexOutOfBoundsException();
+    arrayIndexOutOfBoundsException(4);
   }
 
+  // try/catch
   public static void classCastException(int x) {
     Object i = Integer.valueOf(x);
-    String s = (String) i;
+    try {
+      String s = (String) i;
+    } catch (ClassCastException cce) {
+      cce.printStackTrace();
+    }
   }
 
   public static int nullPointerException(Integer i) {
+    if(i == null) throw new NullPointerException("i can't be 'null'");
     return i;
   }
 
+  //  try/catch
   public static int numberFormatException(String s) {
-    return Integer.valueOf(s);
+    try {
+      return Integer.valueOf(s);
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
+      return 0;
+    }
   }
 
-  public static int arrayIndexOutOfBoundsException() {
+  public static int arrayIndexOutOfBoundsException(int n) {
     int[] arr = {10, 20 ,30};
+    if(n < 0 || n >= arr.length)
+      throw new ArrayIndexOutOfBoundsException("n must be less then " + arr.length + " or greater than 0");
     return arr[4];
   }
 }
